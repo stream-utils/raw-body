38b9c7c01f4745fe47f693ccd9d7c26e5df6fa6c
Raynos <raynos2@gmail.com>
2013-11-26 18:16:28 -0800

    depend & test against readable-stream@1.0.x

--- test.js
+++ test.js
@@ -2,8 +2,7 @@ var assert = require('assert')
 var fs = require('fs')
 var path = require('path')
 var co = require('co')
-var Readable = require('stream').Readable
-  || require('readable-stream').Readable
+var Readable = require('readable-stream').Readable
 
 var getRawBody = require('./')
 
