659b6eb35967e26d084004d0fb6bd8844be557ba
Douglas Christopher Wilson <doug@somethingdoug.com>
2022-02-28 16:45:14 -0500

    docs: update copyright

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*!
  * raw-body
  * Copyright(c) 2013-2014 Jonathan Ong
- * Copyright(c) 2014-2015 Douglas Christopher Wilson
+ * Copyright(c) 2014-2022 Douglas Christopher Wilson
  * MIT Licensed
  */
 
